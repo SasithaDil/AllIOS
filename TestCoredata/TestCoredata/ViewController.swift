dfd1386db351243f1139cbac8ffbdb321cf083e6
Sasitha Dilshan <sasithadilshan77@gmail.com>
2020-02-03 16:49:27 +0530

    All files

---
+++ TestCoredata/TestCoredata/ViewController.swift
@@ -0,0 +1,111 @@
+//
+//  ViewController.swift
+//  TestCoredata
+//
+//  Created by Sasitha Dilshan on 2/1/20.
+//  Copyright © 2020 Sasitha Dilshan. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+class ViewController: UIViewController {
+    
+    var People: [NSManagedObject] = []
+
+    @IBOutlet weak var tableView: UITableView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "Name List"
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+        
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let fetchRequest = NSFetchRequest<NSManagedObject>(entityName: "People")
+        
+        do {
+            People = try managedContext.fetch(fetchRequest)
+        } catch let error as NSError {
+            print("Could not fetch. \(error), \(error.userInfo)")
+        }
+    }
+    
+    @IBAction func addName(_ sender: UIBarButtonItem) {
+        let alert = UIAlertController(title: "New Name", message: "Add a new name", preferredStyle: .alert)
+        
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] action in
+                                        
+          guard let textField = alert.textFields?.first, let nameToSave = textField.text
+            else {
+            return
+         }
+                                        
+            self.save(name: nameToSave)
+            self.tableView.reloadData()
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel)
+        
+        alert.addTextField()
+        
+        alert.addAction(saveAction)
+        alert.addAction(cancelAction)
+        
+        present(alert, animated: true)
+        
+    }
+    func save(name: String) {
+      
+        guard let appDelegate = UIApplication.shared.delegate as? AppDelegate else {
+            return
+        }
+        
+        let managedContext = appDelegate.persistentContainer.viewContext
+        
+        let entity = NSEntityDescription.entity(forEntityName: "People", in: managedContext)!
+        
+        let person = NSManagedObject(entity: entity, insertInto: managedContext)
+        
+      
+        person.setValue(name, forKeyPath: "name")
+        
+        do {
+            try managedContext.save()
+            People.append(person)
+        } catch let error as NSError {
+            print("Could not save. \(error), \(error.userInfo)")
+        }
+    }
+    
+    
+}
+extension ViewController: UITableViewDataSource{
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return People.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+       let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+
+//        cell.textLabel?.text = people[indexPath.row]
+//
+//        return cell
+        
+        let person = People[indexPath.row]
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = person.value(forKeyPath: "name") as? String
+        return cell
+    }
+}
---
+++ unit testing/UnitTesting/UnitTesting/Student.swift
@@ -0,0 +1,9 @@
+//
+//  Student.swift
+//  UnitTesting
+//
+//  Created by Sasitha Dilshan on 2/1/20.
+//  Copyright © 2020 Sasitha Dilshan. All rights reserved.
+//
+
+import Foundation
